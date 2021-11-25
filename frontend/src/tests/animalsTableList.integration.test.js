f746741b569b2590581c4ef7ca8e760ff5c69534
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2021-11-25 11:00:21 -0300

    Mudança de testes para o arquivo de integração

--- frontend/src/tests/animalsTableList.integration.test.js
+++ frontend/src/tests/animalsTableList.integration.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import {useSelector, useDispatch} from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 import { fetchAnimals} from '../redux-toolkit/animals/animalsSlice'
 import AnimalsTable from '../components/AnimalsTable/index'
 import {createMemoryHistory} from 'history';
 import {Router} from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from '../redux/store'
 
 jest.mock("react-redux",() => ({
     ...jest.requireActual("react-redux"),
@@ -63,5 +65,43 @@ describe("Testes de integração Página home + Tabela", () =>{
         expect(page.getByTestId('animals-table')).not.toBeNull()
         expect(page.getByText(mockState.animals.entities["0"].name)).not.toBeNull()
         expect(page.getByText(mockState.animals.entities["1"].name)).not.toBeNull()
+    })
+
+    it('Quando é renderizada com dois animais na lista', () =>{
+        const animal = [{
+            id: "1",
+            name: "Bob",
+            type: "Cachorro",
+            weight: 10.5,
+            date: "2020-01-10"
+        },{
+            id: "2",
+            name: "Nick",
+            type: "Cachorro",
+            weight: 10.5,
+            date: "2020-01-10"
+        }]
+        render(<Provider store={store}><AnimalsTable animals={animal}/></Provider>, {wrapper: MemoryRouter})
+        expect(screen.getByTestId('animals-table')).not.toBeNull()
+        expect(screen.getByText(animal[0].name).textContent).toBe(animal[0].name)
+        expect(screen.getByText(animal[1].name).textContent).toBe(animal[1].name)
+    })
+
+    it('Quando é renderizada com N animais na lista', () =>{
+        const animal = []
+        let counter = 1
+        for(counter=1; counter <= 6; counter++){
+            animal.push({
+                id: counter+"",
+                name: "Animal"+counter,
+                type: "Cachorro",
+                weight: 10.5,
+                date: "2020-01-10"
+            })
+        }
+        render(<Provider store={store}><AnimalsTable animals={animal}/></Provider>, {wrapper: MemoryRouter})
+        expect(screen.getByTestId('animals-table')).not.toBeNull()
+
+        animal.forEach(a => expect(screen.getByText(a.name).textContent).toBe(a.name))
     })
 })
